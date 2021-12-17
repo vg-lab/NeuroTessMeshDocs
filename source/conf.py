648f4cc606fba2b38db3047a4044664831ae8d60
Felix de las Pozas <felixdelaspozas@gmail.com>
2021-12-17 13:12:05 +0100

    Update to version 0.1.1

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Universidad Rey Juan Carlos'
 author = 'Félix de las Pozas Álvarez'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -67,7 +67,7 @@ latex_maketitle = r'''
 \begin{center}
   \includegraphics[width=6cm,height=6cm]{neurotessmesh.png}\\[8ex]
   {\Huge NeuroTessMesh Documentation}\\[4ex]
-  {\Large Version 0.1.0}\\[4ex]
+  {\Large Version 0.1.1}\\[4ex]
   {\Large Juan Jos\'{e} Garc\'{i}a Cantero \& F\'{e}lix de las Pozas \'{A}lvarez}\\[4ex]
   {\Large December 2021}\\[16ex]
   {\Large Visualization \& Graphics Lab, Universidad Rey Juan Carlos}\\[4ex]
